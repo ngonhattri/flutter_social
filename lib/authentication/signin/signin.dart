835b59c3cc2296ef100ab15167eb1cfff7725135
ngonhattri <nhattri1503@gmail.com>
2022-10-18 04:11:33 +0900

    add LoadingSheet at signup and signin screen

--- lib/authentication/signin/signin.dart
+++ lib/authentication/signin/signin.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_social/authentication/google_signin/google_signin_button.dart';
 import 'package:flutter_social/authentication/signin/button.dart';
+import 'package:flutter_social/authentication/signin/controller/signin_controller.dart';
 import 'package:flutter_social/authentication/signin/email_field.dart';
 import 'package:flutter_social/authentication/signin/forgot_password_button.dart';
 import 'package:flutter_social/authentication/signin/or_divider.dart';
 import 'package:flutter_social/authentication/signin/password_field.dart';
+import 'package:flutter_social/components/loading_error.dart';
+import 'package:form_validators/form_validators.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends ConsumerWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<SignInState>(signInProvider, (previous, current) {
+      if (current.status.isSubmissionInProgress) {
+        LoadingSheet.show(context);
+      } else if (current.status.isSubmissionFailure) {
+        Navigator.of(context).pop();
+        ErrorDialog.show(context, "${current.errorMessage}");
+      } else if (current.status.isSubmissionSuccess) {
+        Navigator.of(context).pop();
+      }
+    },);
     return Column(
       children: const [
         EmailField(),
--- lib/authentication/signup/signup.dart
+++ lib/authentication/signup/signup.dart
@@ -5,13 +5,24 @@ import 'package:flutter_social/authentication/signup/controller/signup_controlle
 import 'package:flutter_social/authentication/signup/email_field.dart';
 import 'package:flutter_social/authentication/signup/name_field.dart';
 import 'package:flutter_social/authentication/signup/password_field.dart';
+import 'package:flutter_social/components/loading_error.dart';
+import 'package:form_validators/form_validators.dart';
 
 class SignUp extends ConsumerWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen<SignUpState>(signUpProvider, (previous, current) { });
+    ref.listen<SignUpState>(signUpProvider, (previous, current) {
+      if (current.status.isSubmissionInProgress) {
+        LoadingSheet.show(context);
+      } else if (current.status.isSubmissionFailure) {
+        Navigator.of(context).pop();
+        ErrorDialog.show(context, "${current.errorMessage}");
+      } else if (current.status.isSubmissionSuccess) {
+        Navigator.of(context).pop();
+      }
+    });
     return Column(
       children: const [
         NameField(),
