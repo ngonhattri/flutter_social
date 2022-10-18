53f7c86a19d26ba6d190ed18a33ad2155cec96df
ngonhattri <nhattri1503@gmail.com>
2022-10-18 11:47:44 +0900

    Google Signin

--- lib/authentication/google_signin/google_signin_button.dart
+++ lib/authentication/google_signin/google_signin_button.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'controller/google_signin_controller.dart';
+import 'package:flutter_social/components/loading_error.dart';
 import 'package:flutter_social/components/animated_button.dart';
 
-class GoogleSignInButton extends StatelessWidget {
+class GoogleSignInButton extends ConsumerWidget {
   const GoogleSignInButton({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<GoogleSignInState>(googleSignInProvider, (previous, current) {
+      if(current == GoogleSignInState.loading) {
+        LoadingSheet.show(context);
+      } else if (current == GoogleSignInState.error) {
+        Navigator.of(context).pop();
+        ErrorDialog.show(context, "Google signin failed");
+      } else {
+        Navigator.of(context).pop();
+      }
+    });
     return AnimatedButton(
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 18),
@@ -26,7 +39,9 @@ class GoogleSignInButton extends StatelessWidget {
           ),
         ),
       ),
-      onTap: () {},
+      onTap: () {
+        ref.read(googleSignInProvider.notifier).signInWithGoogle();
+      },
     );
   }
 }
