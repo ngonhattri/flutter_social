26f4b0b1522f329a9f62cf392379f7483dd95f30
ngonhattri <nhattri1503@gmail.com>
2022-10-22 00:51:16 +0900

    Add Forgot_Password func

--- lib/authentication/forgot_password/forgot_password.dart
+++ lib/authentication/forgot_password/forgot_password.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_social/authentication/forgot_password/controller/forgot_password_controller.dart';
 import 'package:flutter_social/components/text_input_field.dart';
+import 'package:form_validators/form_validators.dart';
 
-class ForgotPasswordView extends StatelessWidget {
+class ForgotPasswordView extends ConsumerWidget {
   const ForgotPasswordView({Key? key}) : super(key: key);
 
+  String _getButtonText(FormzStatus status) {
+    if(status.isSubmissionSuccess) {
+      return "requesting";
+    } else if (status.isSubmissionFailure) {
+      return "failed";
+    } else if(status.isSubmissionSuccess) {
+      return "done";
+    } else {
+      return "request";
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final forgotPasswordState = ref.watch(forgotPasswordProvider);
+    final status = forgotPasswordState.status;
     return Scaffold(
       body: Center(
         child: Padding(
@@ -15,24 +32,34 @@ class ForgotPasswordView extends StatelessWidget {
             children: [
               TextInputField(
                 hintText: "Please enter your email",
-                onChanged: (_) {},
+                errorText:
+                    Email.showEmaiErrorMessage(forgotPasswordState.email.error),
+                onChanged: (email) {
+                  ref
+                      .read(forgotPasswordProvider.notifier)
+                      .onEmailChange(email);
+                },
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text("Cancel"),
+                    onPressed: status.isSubmissionInProgress
+                        ? null
+                        : () {
+                            Navigator.of(context).pop();
+                          },
+                    child: const Text("Cancel"),
                   ),
                   TextButton(
-                    onPressed: () {
-
+                    onPressed: status.isSubmissionInProgress ||
+                            status.isSubmissionSuccess
+                        ? null
+                        : () {
+                      ref.read(forgotPasswordProvider.notifier).forgotPassword();
                     },
-                    child: Text("Request"),
+                    child: Text(_getButtonText(status)),
                   ),
-
                 ],
               )
             ],
